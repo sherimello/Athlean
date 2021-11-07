0583494ef10e58dda6425e174c8e8fa6bc4ba811
Robinx0 <binarybyte00@gmail.com>
2021-11-07 17:23:35 +0600

    Healthy recipes

--- lib/widgets/HealthyRecipes.dart
+++ lib/widgets/HealthyRecipes.dart
@@ -247,7 +247,7 @@ class HealthyRecipe extends StatelessWidget {
                           ),
                         ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
